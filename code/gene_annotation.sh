b29716a5f9937713de1c29aeb1584a9450f1cc5a
endeneon <siwei.zhang84@gmail.com>
2021-10-19 13:44:16 -0500

    Update gene_annotation.sh

--- code/gene_annotation.sh
+++ code/gene_annotation.sh
@@ -5,7 +5,7 @@ Gene_Loc_File="/data/your-name/magma-aux-files/Gene_Loc_File"
 Output_Prefix="miR137_100k_20k"
 
 magma \
-	--annotate window=100,20\
+	--annotate window=100,20 \
 	--snp-loc $SNP_Loc_File \
-	-- gene-loc $Gene_Loc_File\
-	-- out $Output_Prefix
+	--gene-loc $Gene_Loc_File \
+	--out $Output_Prefix
